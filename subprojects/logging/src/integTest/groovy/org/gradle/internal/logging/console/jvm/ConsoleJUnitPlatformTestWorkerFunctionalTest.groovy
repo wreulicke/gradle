30ca9b3245e9cbc6e591855024b36a9a398cb4f5
Jendrik Johannes <jendrik@gradle.com>
2019-05-28 09:41:05 +0200

    Let logging tests not use deprecated configurations

--- subprojects/logging/src/integTest/groovy/org/gradle/internal/logging/console/jvm/ConsoleJUnitPlatformTestWorkerFunctionalTest.groovy
+++ subprojects/logging/src/integTest/groovy/org/gradle/internal/logging/console/jvm/ConsoleJUnitPlatformTestWorkerFunctionalTest.groovy
@@ -39,7 +39,7 @@ class ConsoleJUnitPlatformTestWorkerFunctionalTest extends AbstractConsoleJvmTes
         """
             tasks.withType(Test) {
                 useJUnitPlatform()
-            }
+            }   
         """
     }
 }
